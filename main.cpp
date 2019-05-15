be66636863cb7549effcb6f6f8e47d4029dc71a8
Dr Pepper <chibisaincrada@gmail.com>
2019-05-15 18:35:43 +0300

    perc_is_here

--- main.cpp
+++ main.cpp
@@ -18,31 +18,67 @@ int* targs_train;
 int* targs_test;
 float** emg_test;
 float** emg_train;
-vector<deque<vector<float>>> data_l_inp;
+vector<vector<vector<float>>> data_l_inp;//[classes][samples][channels]
 vector<vector<float>> classes;
 int N_test,N_train;
 int chans[3]={3,6,7};
 
 int main(int argc, char *argv[])
 {
+
     Histogram hist1, hist2;
     openFile(string(argv[1]),emg_train,targs_train, N_train);
     openFile(string(argv[2]),emg_test,targs_test, N_test);
-    cout<<argv[1]<<endl;
+
 
     vector<int> constr;
     constr.push_back(hist1.N2+hist2.N2);
-    constr.push_back(5);//10
+    constr.push_back(7);//10
     perc=new perceptron(constr);
 
-    for(int j=0;j<N_train;j++)
+
+    //data_l_inp.resize(hist1.N2+hist2.N2);
+    data_l_inp.resize(7);
+
+    int cnt=0;
+    for(int j=0;j<(N_train);j++)
     {
+        cnt++;
         hist1.increment(emg_train[0][j],emg_train[1][j]);
         hist2.increment(emg_train[0][j],emg_train[2][j]);
+//        if(cnt==10)
+        {
+
+            cnt=0;
+            vector<float> vv=hist1.b;
+            vv.insert(vv.end(),hist2.b.begin(),hist2.b.end());
+            cout<<vv.size()<<endl;
+            data_l_inp[targs_train[j]-1].push_back(vv);
+        }
     }
+    //    vector<float> aa, bb;
+    //    aa.push_back(1);
+    //    aa.push_back(2);
+    //    bb=aa;
+    //    bb[1]=3;
+    //    aa[1]+=10;
+    //    aa.insert(aa.end(),bb.begin(),bb.end());
+    //    cout<<aa[3];
+    int class_i;
+    for (int i=0;i<7;i++)
+        cout<<data_l_inp[i].size()<<endl;
 
-
-
+    int indent=200;
+    int sample_i;
+    for(int k=0;k<30000;k++)
+    {
+        class_i=rand()%7;
+        vector<float> targs_v;
+        sample_i=k%(data_l_inp[class_i].size()-indent)+indent;
+        targs_v.resize(7);
+        targs_v[class_i]=1;
+        perc->learn1(data_l_inp[class_i][sample_i],targs_v);
+    }
 
 
 }
@@ -63,10 +99,11 @@ void openFile(string str, float**& x, int*& targs, int& N)
 
         QStringList s_list;
         while (!line.isNull())
+
         {
             s_list.push_back(line);
             line = in.readLine();
-            //           cout<<s_list.size()<<'\n';
+
         }
         N=s_list.size();
 
@@ -75,7 +112,6 @@ void openFile(string str, float**& x, int*& targs, int& N)
         {
             x[i]=new float[N];
         }
-
         targs=new int[N];
 
 
@@ -94,13 +130,15 @@ void openFile(string str, float**& x, int*& targs, int& N)
             targs[j]=vstr[9].toFloat();
         }
 
-        for(int j=0;j<N;j++)
-        {
-            for (int i=0;i<3;i++)
-                cout<<x[i][j]<<"  ";
-            cout<<targs[j];
-            cout<<endl;
-        }
+        //        for(int j=0;j<N;j++)
+        //        {
+        //            for (int i=0;i<3;i++)
+        //                cout<<x[i][j]<<"     ";//5 spaces
+        //            cout<<targs[j];
+        //            cout<<endl;
+        //        }
+        //        cout<<endl;
+
     }
 }
 
